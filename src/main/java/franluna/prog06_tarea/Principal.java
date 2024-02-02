f5ef6bc2f56125001edceefc8dd8c1582a040788
Fluna29 <fluna293107@gmail.com>
2024-02-02 18:55:42 +0100

    Subida Inicial

---
+++ src/main/java/franluna/prog06_tarea/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package franluna.prog06_tarea;
+
+/**
+ *
+ * @author fluna
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
